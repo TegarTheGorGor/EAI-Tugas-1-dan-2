38fe2a3c2296602b2839f72f6c52e18fe759be5b
TegarTheGorGor <tegardildil@gmail.com>
2025-03-27 17:48:06 +0700

    second commit

--- index.php
+++ index.php
@@ -28,6 +28,24 @@
 </head>
 <body>
 
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="#">Tugas 1 dan 2 EAI Mochamad Tegar Abdillah 120221048</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="GetApi.php">Sample data API</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+
 <div class="container mt-5">
     <h1 class="text-center mb-4">Koleksi Buku</h1>
     <div class="row">
